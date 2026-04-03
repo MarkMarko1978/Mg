0af0903d9befc69367369f3d68d48e516b2f8b89
Swag <markosyanmarkmarkmos@gmail.com>
2026-04-03 09:22:48 +0300

    Update filemg.py

--- filemg.py
+++ filemg.py
@@ -114,6 +114,37 @@ async def filemg3(ctx):
     await ctx.send(embed=embed, view=FileMG3View())
     await ctx.message.delete()
 
+class FileMG4Select(Select):
+    def __init__(self):
+        options = [
+            discord.SelectOption(label="Locker Builder", description="Нажми чтобы получить ссылку", emoji="📦"),
+        ]
+        super().__init__(placeholder="Выберите инструмент...", options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        links = {
+            "Locker Builder": "https://drive.google.com/file/d/1NMQqTxo1-d1WAh-68MgAn1eASdqANdN2/view?usp=sharing",
+        }
+        selected = self.values[0]
+        link = links[selected]
+        embed = discord.Embed(title=f"📥 {selected}", description=f"[**Скачать {selected}**]({link})", color=discord.Color.purple())
+        embed.set_footer(text="FileMG | by lort")
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+class FileMG4View(View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.add_item(FileMG4Select())
+
+@bot.command(name="filemg4")
+@mod_check()
+async def filemg4(ctx):
+    embed = discord.Embed(title="Выберите инструмент из категории", description="Выберите вариант в меню ниже, чтобы просмотреть подробную информацию и получить ссылку для скачивания.", color=discord.Color.purple())
+    embed.set_image(url="https://media.discordapp.net/attachments/1483812220499398717/1489194360854675488/standard_4.gif?ex=69cf87d3&is=69ce3653&hm=238fb907ff1c006165275ad3a542139c8a1dd99ffe7f332e238c0bf5c5daaf52&=")
+    embed.set_footer(text="FileMG | by lort")
+    await ctx.send(embed=embed, view=FileMG4View())
+    await ctx.message.delete()
+
 @bot.event
 async def on_ready():
     print(f"Бот запущен: {bot.user}")
