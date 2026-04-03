46e16989e13b4da870d0dee45cd63f10811e7ffb
Swag <markosyanmarkmarkmos@gmail.com>
2026-04-03 09:14:19 +0300

    Update filemg.py

--- filemg.py
+++ filemg.py
@@ -32,11 +32,7 @@ class FileMGSelect(Select):
         }
         selected = self.values[0]
         link = links[selected]
-        embed = discord.Embed(
-            title=f"📥 {selected}",
-            description=f"[**Скачать {selected}**]({link})",
-            color=discord.Color.purple()
-        )
+        embed = discord.Embed(title=f"📥 {selected}", description=f"[**Скачать {selected}**]({link})", color=discord.Color.purple())
         embed.set_footer(text="FileMG | by lort")
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
@@ -48,11 +44,7 @@ class FileMGView(View):
 @bot.command(name="filemg")
 @mod_check()
 async def filemg(ctx):
-    embed = discord.Embed(
-        title="Выберите инструмент из категории",
-        description="Выберите вариант в меню ниже, чтобы просмотреть подробную информацию и получить ссылку для скачивания.",
-        color=discord.Color.purple()
-    )
+    embed = discord.Embed(title="Выберите инструмент из категории", description="Выберите вариант в меню ниже, чтобы просмотреть подробную информацию и получить ссылку для скачивания.", color=discord.Color.purple())
     embed.set_image(url="https://media.discordapp.net/attachments/1483812220499398717/1489194360854675488/standard_4.gif?ex=69cf87d3&is=69ce3653&hm=238fb907ff1c006165275ad3a542139c8a1dd99ffe7f332e238c0bf5c5daaf52&=")
     embed.set_footer(text="FileMG | by lort  пароль - EzSq")
     await ctx.send(embed=embed, view=FileMGView())
@@ -73,11 +65,7 @@ class FileMG2Select(Select):
         }
         selected = self.values[0]
         link = links[selected]
-        embed = discord.Embed(
-            title=f"📥 {selected}",
-            description=f"[**Скачать {selected}**]({link})",
-            color=discord.Color.purple()
-        )
+        embed = discord.Embed(title=f"📥 {selected}", description=f"[**Скачать {selected}**]({link})", color=discord.Color.purple())
         embed.set_footer(text="FileMG | by lort")
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
@@ -89,14 +77,41 @@ class FileMG2View(View):
 @bot.command(name="filemg2")
 @mod_check()
 async def filemg2(ctx):
-    embed = discord.Embed(
-        title="Выберите инструмент из категории",
-        description="Выберите вариант в меню ниже, чтобы просмотреть подробную информацию и получить ссылку для скачивания.",
-        color=discord.Color.purple()
-    )
+    embed = discord.Embed(title="Выберите инструмент из категории", description="Выберите вариант в меню ниже, чтобы просмотреть подробную информацию и получить ссылку для скачивания.", color=discord.Color.purple())
     embed.set_image(url="https://media.discordapp.net/attachments/1483812220499398717/1489194360854675488/standard_4.gif?ex=69cf87d3&is=69ce3653&hm=238fb907ff1c006165275ad3a542139c8a1dd99ffe7f332e238c0bf5c5daaf52&=")
     embed.set_footer(text="FileMG | by lort")
     await ctx.send(embed=embed, view=FileMG2View())
+    await ctx.message.delete()
+
+class FileMG3Select(Select):
+    def __init__(self):
+        options = [
+            discord.SelectOption(label="Xbinder", description="Нажми чтобы получить ссылку", emoji="📦"),
+        ]
+        super().__init__(placeholder="Выберите инструмент...", options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        links = {
+            "Xbinder": "https://drive.google.com/file/d/1Z-zKWvcVii0lt7DfhgFql8_sGQoxqKvJ/view?usp=sharing",
+        }
+        selected = self.values[0]
+        link = links[selected]
+        embed = discord.Embed(title=f"📥 {selected}", description=f"[**Скачать {selected}**]({link})", color=discord.Color.purple())
+        embed.set_footer(text="FileMG | by lort")
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+class FileMG3View(View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.add_item(FileMG3Select())
+
+@bot.command(name="filemg3")
+@mod_check()
+async def filemg3(ctx):
+    embed = discord.Embed(title="Выберите инструмент из категории", description="Выберите вариант в меню ниже, чтобы просмотреть подробную информацию и получить ссылку для скачивания.", color=discord.Color.purple())
+    embed.set_image(url="https://media.discordapp.net/attachments/1483812220499398717/1489194360854675488/standard_4.gif?ex=69cf87d3&is=69ce3653&hm=238fb907ff1c006165275ad3a542139c8a1dd99ffe7f332e238c0bf5c5daaf52&=")
+    embed.set_footer(text="FileMG | by lort")
+    await ctx.send(embed=embed, view=FileMG3View())
     await ctx.message.delete()
 
 @bot.event
